7c9d41797d6434b7b21de9c1da5d797dd691f4ba
beas <148710393+bellbuuu@users.noreply.github.com>
2024-06-26 15:45:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const recipe=document.querySelector('.one');
+const recipe=document.querySelector('.recipe');
 const weather=document.querySelector('.two');
 const pong=document.querySelector('.three');
 const snake=document.querySelector('.four');
@@ -13,6 +13,36 @@ const yui=document.querySelector('.yui');
 const yui2=document.querySelector('.yui2');
 const light=document.querySelector('.light');
 const light2=document.querySelector('.light2');
+
+
+recipe.addEventListener('click', ()=>{
+    var url=["https://github.com/bellbuuu/recipeapp"];
+    window.open(url);
+})
+weather.addEventListener('click', ()=>{
+    var url=["https://github.com/bellbuuu/weatherapp"];
+    window.open(url);
+})
+pong.addEventListener('click', ()=>{
+    var url=["https://github.com/bellbuuu/pong-game"];
+    window.open(url);
+})
+snake.addEventListener('click', ()=>{
+    var url=["https://github.com/bellbuuu/simple-snake-game-"];
+    window.open(url);
+})
+vowel.addEventListener('click', ()=>{
+    var url=["https://github.com/bellbuuu/vowel-counter"];
+    window.open(url);
+})
+calc.addEventListener('click', ()=>{
+    var url=["https://github.com/bellbuuu/calculator"];
+    window.open(url);
+})
+portfolio.addEventListener('click', ()=>{
+    var url=["https://github.com/bellbuuu/beas.ink"];
+    window.open(url);
+})
 
 document.getElementsByClassName("confetti")[0].addEventListener("click", ()=>{
     let canvas= document.createElement("canvas");
@@ -58,31 +88,5 @@ light2.addEventListener('click',()=>{
     sound4.pause();
 })
 
-recipe.addEventListener('click', ()=>{
-    var url=["https://github.com/bellbuuu/recipeapp"];
-    window.open(url);
-})
-weather.addEventListener('click', ()=>{
-    var url=["https://github.com/bellbuuu/weatherapp"];
-    window.open(url);
-})
-pong.addEventListener('click', ()=>{
-    var url=["https://github.com/bellbuuu/pong-game"];
-    window.open(url);
-})
-snake.addEventListener('click', ()=>{
-    var url=["https://github.com/bellbuuu/simple-snake-game-"];
-    window.open(url);
-})
-vowel.addEventListener('click', ()=>{
-    var url=["https://github.com/bellbuuu/vowel-counter"];
-    window.open(url);
-})
-calc.addEventListener('click', ()=>{
-    var url=["https://github.com/bellbuuu/calculator"];
-    window.open(url);
-})
-portfolio.addEventListener('click', ()=>{
-    var url=["https://github.com/bellbuuu/beas.ink"];
-    window.open(url);
-})
+
+
