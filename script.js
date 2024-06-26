544e6901e1efeffbe1be20608096422df4898ee6
beas <148710393+bellbuuu@users.noreply.github.com>
2024-06-26 15:55:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,3 @@
-const recipe=document.querySelector('.recipe');
-const weather=document.querySelector('.two');
-const pong=document.querySelector('.three');
-const snake=document.querySelector('.four');
-const vowel=document.querySelector('.five');
-const portfolio=document.querySelector('.seven');
-const calc=document.querySelector('.six');
-const gluesong=document.querySelector('.gluesong');
-const gluesong2=document.querySelector('.gluesong2');
-const wow=document.querySelector('.wow');
-const wow2=document.querySelector('.wow2');
-const yui=document.querySelector('.yui');
-const yui2=document.querySelector('.yui2');
-const light=document.querySelector('.light');
-const light2=document.querySelector('.light2');
-
-
-recipe.addEventListener('click', ()=>{
-    var url=["https://github.com/bellbuuu/recipeapp"];
-    window.open(url);
-})
-weather.addEventListener('click', ()=>{
-    var url=["https://github.com/bellbuuu/weatherapp"];
-    window.open(url);
-})
-pong.addEventListener('click', ()=>{
-    var url=["https://github.com/bellbuuu/pong-game"];
-    window.open(url);
-})
-snake.addEventListener('click', ()=>{
-    var url=["https://github.com/bellbuuu/simple-snake-game-"];
-    window.open(url);
-})
-vowel.addEventListener('click', ()=>{
-    var url=["https://github.com/bellbuuu/vowel-counter"];
-    window.open(url);
-})
-calc.addEventListener('click', ()=>{
-    var url=["https://github.com/bellbuuu/calculator"];
-    window.open(url);
-})
-portfolio.addEventListener('click', ()=>{
-    var url=["https://github.com/bellbuuu/beas.ink"];
-    window.open(url);
-})
 
 document.getElementsByClassName("confetti")[0].addEventListener("click", ()=>{
     let canvas= document.createElement("canvas");
@@ -55,6 +10,16 @@ document.getElementsByClassName("confetti")[0].addEventListener("click", ()=>{
     particleCount:200;
 
 })
+
+const gluesong=document.querySelector('.gluesong');
+const gluesong2=document.querySelector('.gluesong2');
+const wow=document.querySelector('.wow');
+const wow2=document.querySelector('.wow2');
+const yui=document.querySelector('.yui');
+const yui2=document.querySelector('.yui2');
+const light=document.querySelector('.light');
+const light2=document.querySelector('.light2');
+
 var sound1=new Audio();
 sound1.src="yui.mp3";
 yui.addEventListener('click',()=>{
