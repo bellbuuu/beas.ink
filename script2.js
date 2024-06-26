f90da5b4307e7c2278a13d64836b01a8d2c0bc23
beas <148710393+bellbuuu@users.noreply.github.com>
2024-06-26 15:56:24 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,36 @@
+const recipe=document.querySelector('.one');
+const weather=document.querySelector('.two');
+const pong=document.querySelector('.three');
+const snake=document.querySelector('.four');
+const vowel=document.querySelector('.five');
+const calc=document.querySelector('.six');
+const portfolio=document.querySelector('.seven');
+
+recipe.addEventListener('click', ()=>{
+    var url=["https://github.com/bellbuuu/recipeapp"];
+    window.open(url);
+})
+weather.addEventListener('click', ()=>{
+    var url=["https://github.com/bellbuuu/weatherapp"];
+    window.open(url);
+})
+pong.addEventListener('click', ()=>{
+    var url=["https://github.com/bellbuuu/pong-game"];
+    window.open(url);
+})
+snake.addEventListener('click', ()=>{
+    var url=["https://github.com/bellbuuu/simple-snake-game-"];
+    window.open(url);
+})
+vowel.addEventListener('click', ()=>{
+    var url=["https://github.com/bellbuuu/vowel-counter"];
+    window.open(url);
+})
+calc.addEventListener('click', ()=>{
+    var url=["https://github.com/bellbuuu/calculator"];
+    window.open(url);
+})
+portfolio.addEventListener('click', ()=>{
+    var url=["https://github.com/bellbuuu/beas.ink"];
+    window.open(url);
+})
